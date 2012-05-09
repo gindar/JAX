095f8bb485c0b39b37d81fbc2acec290f0ea5952
Gindar <gindar@zamraky.cz>
2012-05-09 10:22:18 +0200

    build update

--- build.sh
+++ build.sh
@@ -10,13 +10,17 @@ fi
 
 cat src/core.js src/element.js src/deco.js src/dom.js src/dombuilder.js src/fx.js > build/jax.js
 cat deps/timekeeper.js deps/interpolator.js build/jax.js > build/jax-all.js
+
+cd deps/jak
+./makejaklight.sh
+cd ../..
 cat deps/jak.light.js deps/timekeeper.js deps/interpolator.js build/jax.js > build/jaxsa.js
 echo "JAX.version='$VERSION';" >> build/jax.js
 echo "JAX.version='$VERSION-alldeps';" >> build/jax-all.js
 echo "JAX.version='$VERSION-sa';" >> build/jaxsa.js
 
 if [ "$1" = "release" ]; then
-	zip build/jax-$VERSION.zip deps/* test/* doc/* build/jax.js build/jax-all.js build/jaxsa.js README.md
+	zip build/jax-$VERSION.zip deps/*.js test/* doc/* build/jax.js build/jax-all.js build/jaxsa.js README.md
 fi
 
 if [ "$1" = "devel" ]; then
